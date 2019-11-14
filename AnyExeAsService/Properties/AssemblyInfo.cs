e1e7fa62c05d5305a7758bf11d2583329220934e
SOCAR <marek.malecki@gmail.com>
2019-11-14 23:33:59 +0100

    Initial commit. Move of code from private svn repo to github.

---
+++ AnyExeAsService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AnyExeAsService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnyExeAsService")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2c9f2da-21d0-41c3-81b8-fe5f1c25b63d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AnyExeAsService/Service1.cs
@@ -0,0 +1,95 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace AnyExeAsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Process p = null;
+        ILog log = LogManager.GetLogger("applog");
+        public Service1()
+        {
+            InitializeComponent();
+            XmlConfigurator.Configure();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.Arguments = Service1.ExeParameters;
+                psi.FileName = Service1.ExeToRun;
+                if (String.IsNullOrEmpty(Service1.ExeWorkDir))
+                {
+                    try
+                    {
+                        FileInfo fi = new FileInfo(psi.FileName);
+                        psi.WorkingDirectory = fi.Directory.FullName;
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error("OnStart_ExeWorkDir: " + ex.Message);
+                    }
+
+                }
+                else
+                    psi.WorkingDirectory = Service1.ExeWorkDir;
+
+                p = new Process();
+                p.StartInfo = psi;
+                p.Start();
+            }
+            catch (Exception ex)
+            {
+                log.Error("OnStart: " + ex.Message);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                p.Kill();
+            }
+            catch(Exception ex)
+            {
+                log.Error("OnStop: " + ex.Message);
+            }
+        }
+
+        public static string ExeToRun
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["exeToRun"].ToString();
+            }
+        }
+
+        public static string ExeParameters
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["exeParameters"].ToString();
+            }
+        }
+
+        public static string ExeWorkDir
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["exeWorkingDirectory"].ToString();
+            }
+        }
+    }
+}
